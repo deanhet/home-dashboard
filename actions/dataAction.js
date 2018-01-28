7da223e41a38924a29500221995d246b1303bbb7
Dean Hetherington <deanhet@gmail.com>
2018-01-28 20:09:08 +0000

    Trying to be more defensive for when spotify actions fail

--- actions/dataAction.js
+++ actions/dataAction.js
@@ -4,7 +4,7 @@ const data = async (url, config = {
   method: 'GET'
 }) => {
   const data = await fetch(url, config)
-  if (!data.ok || data._bodyText === "") return
+  if (!data.ok || data._bodyText === '') return false
   const response = await data.json()
   return response
 }

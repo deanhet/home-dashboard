4d23dfa2eb33530f31c4d2cb28a952e52b899736
Dean Hetherington <deanhet@gmail.com>
2018-02-25 16:06:04 +0000

    Fix error in fetching TV schedule

--- state/actions/tv.js
+++ state/actions/tv.js
@@ -5,7 +5,7 @@ export const UPDATE_SHOWS = 'tv/UPDATE_SHOWS'
 
 export function updateTV() {
   return async dispatch => {
-    const request = await data(`http://${keys.tv.ip}/api/${keys.tv.key}/?cmd=future&type=today|soon|later&paused=1`)
+    const request = await data(`${keys.tv.ip}/api/${keys.tv.key}/?cmd=future&type=today|soon|later&paused=1`)
     const { later, soon, today } = request.data
     dispatch({ type: UPDATE_SHOWS, data: [... today, ... soon, ... later] })
   }

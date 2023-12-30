03db8423cac679c43d8ab1ec02570b729c5e7fe2
Dean Hetherington <deanhet@hey.com>
2023-12-30 19:06:43 +0000

    Update pods, fix typo

--- state/actions/weather.js
+++ state/actions/weather.js
@@ -5,7 +5,7 @@ export const UPDATE_WEATHER = 'weather/UPDATE_WEATHER'
 
 export function updateWeather() {
   return async dispatch => {
-    const weather = await data(`https://api.pirateweather.net/forecast/${keys.weather.key}/${keys.homeCoord}?units=uk2`)
+    const weather = await data(`https://api.pirateweather.net/forecast/${keys.weather.key}/${keys.homeCoord}?units=uk`)
 
     if (weather) {
       dispatch({ type: UPDATE_WEATHER, data: weather })
